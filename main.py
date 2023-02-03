40ac82b9af99c98eba9828b430c7d1a656e410db
Samuel Thorén <samuel.thoren@astazero.com>
2023-02-03 13:23:09 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+from simulation_info import Simulation, Car, Path, Fargs, animate
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+def main():
+    
+    sim  = Simulation()
+    path = Path()
+    car  = Car(path.px[0], path.py[0], path.pyaw[0], path.px, path.py, path.pyaw, sim.dt)
+
+    interval = sim.dt * 10**3
+
+    fig = plt.figure()
+    ax = plt.axes()
+    ax.set_aspect('equal')
+
+    road = plt.Circle((0, 0), 50, color='gray', fill=False, linewidth=30)
+    ax.add_patch(road)
+    ax.plot(path.px, path.py, '--', color='gold')
+
+    empty              = ([], [])
+    target,            = ax.plot(*empty, '+r')
+    car_outline,       = ax.plot(*empty, color=car.colour)
+    front_right_wheel, = ax.plot(*empty, color=car.colour)
+    rear_right_wheel,  = ax.plot(*empty, color=car.colour)
+    front_left_wheel,  = ax.plot(*empty, color=car.colour)
+    rear_left_wheel,   = ax.plot(*empty, color=car.colour)
+    rear_axle,         = ax.plot(car.x, car.y, '+', color=car.colour, markersize=2)
+    annotation         = ax.annotate(f'{car.x:.1f}, {car.y:.1f}', xy=(car.x, car.y + 5), color='black', annotation_clip=False)
+
+    fargs = [Fargs(
+        ax=ax,
+        sim=sim,
+        path=path,
+        car=car,
+        car_outline=car_outline,
+        front_right_wheel=front_right_wheel,
+        front_left_wheel=front_left_wheel,
+        rear_right_wheel=rear_right_wheel,
+        rear_left_wheel=rear_left_wheel,
+        rear_axle=rear_axle,
+        annotation=annotation,
+        target=target
+    )]
+
+    _ = FuncAnimation(fig, animate, frames=sim.frames, init_func=lambda: None, fargs=fargs, interval=interval, repeat=sim.loop)
+    # anim.save('animation.gif', writer='imagemagick', fps=50)
+    
+    plt.grid()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
