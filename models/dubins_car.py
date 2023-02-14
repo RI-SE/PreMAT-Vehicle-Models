194c079bce52d1bee02fa7557468efeafb8c4d32
Samuel Thorén <samuel.thoren@astazero.com>
2023-02-14 14:07:33 +0100

    Dubins Car

--- models/dubins_car.py
+++ models/dubins_car.py
@@ -9,7 +9,11 @@ from typing import Tuple
 class DubinsCar(Model):
 
   def update(self, x: float, y: float, yaw: float, velocity: float, acceleration: float, steering_angle: float) -> Tuple[float, ...]:
-    new_velocity = velocity * self.delta_time * acceleration
-    # angular_velocity = 
+    new_velocity = velocity + self.delta_time * acceleration
+    angular_velocity = velocity * sin(steering_angle)
     new_x   = x + velocity*cos(yaw)*self.delta_time
     new_y   = y + velocity*sin(yaw)*self.delta_time
+    new_yaw = normalise_angle(yaw + angular_velocity*self.delta_time)
+
+
+    return new_x, new_y, new_yaw, new_velocity, steering_angle, angular_velocity
