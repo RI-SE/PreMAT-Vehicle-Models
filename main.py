23f195c4fa81c2e0a690e3d3a928436203ca2b40
Samuel Thorén <samuel.thoren@astazero.com>
2023-02-17 10:07:49 +0100

    Moved VehicleSimulation to an own file

--- main.py
+++ main.py
@@ -1,74 +1,8 @@
-from simulation_info import Simulation, Car, Path, Fargs, animate
-from matplotlib import pyplot as plt
-from matplotlib.animation import FuncAnimation
-
-import numpy as np
-
-class VehicleSimulation:
-    def __init__(self, data: str, model: str, vmax: float, constant_velocity: bool = False):
-        self.path = Path(data)
-        self.sim  = Simulation(self.path.delta_time, max_size_x = 5, max_size_y = 5, frames = len(self.path.acceleration))
-        self.car  = Car(self.path.px[0], self.path.py[0], self.path.pyaw[0],
-                        self.path.px, self.path.py, self.path.pyaw, self.path.acceleration,
-                        self.path.delta_time, self.path.time[-1], vmax, model, constant_velocity)
-
-
-    def show_animation(self):
-        path = self.path
-        sim = self.sim
-        car = self.car
-
-        interval = sim.dt * 10**3
-
-        fig = plt.figure()
-        ax = plt.axes()
-        ax.set_aspect('equal')
-
-        ax.plot(path.px, path.py, '--', color='gold')
-
-        empty              = ([], [])
-        target,            = ax.plot(*empty, '+r')
-        car_outline,       = ax.plot(*empty, color=car.colour)
-        front_right_wheel, = ax.plot(*empty, color=car.colour)
-        rear_right_wheel,  = ax.plot(*empty, color=car.colour)
-        front_left_wheel,  = ax.plot(*empty, color=car.colour)
-        rear_left_wheel,   = ax.plot(*empty, color=car.colour)
-        rear_axle,         = ax.plot(car.x, car.y, '+', color=car.colour, markersize=2)
-        annotation         = ax.annotate(f'{car.x:.1f}, {car.y:.1f}', xy=(car.x, car.y + 5), color='black', annotation_clip=False)
-
-        fargs = [Fargs(
-            ax=ax,
-            sim=sim,
-            path=path,
-            car=car,
-            car_outline=car_outline,
-            front_right_wheel=front_right_wheel,
-            front_left_wheel=front_left_wheel,
-            rear_right_wheel=rear_right_wheel,
-            rear_left_wheel=rear_left_wheel,
-            rear_axle=rear_axle,
-            annotation=annotation,
-            target=target
-        )]
-
-        _ = FuncAnimation(fig, animate, frames=car.gen, init_func=lambda: None, fargs=fargs, interval=interval, repeat=sim.loop)
-        # anim.save('animation.gif', writer='imagemagick', fps=50)
-        
-        plt.grid()
-        plt.show()
-
-
-    def calculate_error(self):
-        while self.car.continue_animation:
-            self.car.drive()
-        
-        print(f"Sum, all cross-track terms: {sum(self.car.all_crosstrack_errors[0:-1])}")
-        print(f"Mean, all cross-track terms: {np.mean(self.car.all_crosstrack_errors[0:-1])}")
-        print(f"Standard deviation, all cross-track terms: {np.std(self.car.all_crosstrack_errors[0:-1])}")
-
+from vehicle_simulation import VehicleSimulation
 
 if __name__ == '__main__':
-    data = "data/data5.csv"
+
+    data = "data/data4.csv"
     model = "BicycleModel"
     vmax = 3.75
     constant_velocity = False
