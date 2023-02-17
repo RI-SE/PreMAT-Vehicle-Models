a30091dc9b5d360ab3014d73bd697f5411574171
Samuel Thorén <samuel.thoren@astazero.com>
2023-02-17 10:58:43 +0100

    Method for calculating and printing errors

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from vehicle_simulation import VehicleSimulation
 
+
+def calculate_errors(dataList: list, models: list, vmax: float) -> list:
+    for model in models:
+        for data in dataList:
+            print("Data: " + data)
+            print("Model: " + model)
+            simulation = VehicleSimulation(data, model, vmax, True)
+            simulation.calculate_error()
+            print("")    
+    
+
 if __name__ == '__main__':
 
-    data = "data/data4.csv"
-    model = "BicycleModel"
+    data = ["data/data1.csv", "data/data3.csv", "data/data5.csv"]
+    models = ["BicycleModel", "DubinsCar"]
     vmax = 3.75
-    constant_velocity = False
+    calculate_errors(data, models, vmax)
 
-    main = VehicleSimulation(data, model, vmax, constant_velocity)
-    main.show_animation()
-    main.calculate_error()
+    # a = VehicleSimulation("data/data5.csv", "DubinsCar", 3.75, True)
+    # a.show_animation()
+
+
+    
